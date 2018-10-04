c0424b3369d8ca6ebda6e681d48cf849b39ff4b7
Corey Craddock <corey.craddock@gmail.com>
2018-10-03 20:24:35 -0600

    Created loading icon while app is loading weather data

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+//*********** Responsible for controling UI elements like 'menu' ****************/
+const UI = (function() {
+
+    const showApp = () => {
+        document.querySelector('#app-loader').classList.add('display-none');
+        document.querySelector('main').removeAttribute('hidden')
+    };
+
+    const loadApp = () => {
+        document.querySelector('#app-loader').classList.remove('display-none');
+        document.querySelector('main').setAttribute('hidden', 'true');
+    };
+
+    return {
+        showApp,
+        loadApp
+    }
+})();
+
+//*********** Init ****************/
+window.onload = function() {
+    UI.showApp();
+}
